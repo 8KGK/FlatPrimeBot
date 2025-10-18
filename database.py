dae7d986c2a3a41355c584fa4a38e262a929304e
Максим <leskomaksim000@gmail.com>
2025-10-18 18:08:08 +0300

    Remove hardcoded password from database connection
    Removed hardcoded database password for security.

--- database.py
+++ database.py
@@ -17,7 +17,7 @@ def get_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="5585",
+        password="",
         database="Flat",
         autocommit=True
     )
